060c53ccd75cfcbab7bf92f9bad698c770c7739f
Giuseppe11S <senagiuseppe11@gmail.com>
2025-11-14 21:00:58 +0100

    Added style in NavBar.jsx for navbar layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,47 @@
 // NavBar presente in tutte le pagine 
+import { Link } from "react-router-dom"
+// import per le icone 
+import { ShoppingCart, Heart, Search, Badge } from "lucide-react";
 
 export default function NavBar() {
   return (
     <>
-     <h1>NavBar</h1>
+     <nav className="container d-flex justify-content-between p-3">
+      {/* Logo */}
+       <h1 className="navbar__logo fs-4 ">NonServeaNiente</h1>
+       {/* Elementi di navigazione con icone annesse */}
+        <ul className="d-flex justify-content-between gap-5 ">
+          <li>
+            <Link >
+                <Search 
+                size={18}
+                color="#000"/>
+          </Link>
+          </li>
+          <li>
+            <Link >
+                <Heart 
+                size={18}
+                color="#000"/>
+            </Link>
+            </li>
+          <li>
+            <div className="position-relative">
+            <Link>
+              <ShoppingCart 
+              size={18}
+              color="#000"
+              />
+             <Badge 
+             size={22}
+             className="position-absolute bottom-50"/>
+             <span 
+             className="position-absolute bottom-50 badge text-black fs-10">5</span>
+            </Link>
+            </div>
+          </li>
+        </ul>
+     </nav>
     </>
   )
 }
