37f0d3ef6c8baabfd3f5636cf86a4fc8b147c2f1
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-11-15 10:36:48 +0100

    Add product detail page with basic layout and standalone CSS

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,43 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import "./ProductPage.css"
 
+export default function ProductPage() {
+  const { id } = useParams()
 
+  const [product, setProduct] = useState(null)
 
+  useEffect(() => {
+    // Placeholder temporaneo
+    const fakeProduct = {
+      id,
+      name: "Nome del prodotto",
+      price: 49.99,
+      description:
+        "Descrizione a cazzo di cane provvisoria ",
+      image: "???",
+    }
 
-export default function HomePage() {
+    setProduct(fakeProduct)
+  }, [id])
+
+  if (!product) return <h2>Prodotto non trovato...</h2>
+
   return (
-    <>
-    <h1>Product</h1>
-    
-    </>
+    <div className="product-container">
+      <div className="product-image-box">
+        <img className="product-image" src={product.image} alt={product.name} />
+      </div>
+
+      <div className="product-info-box">
+        <h1 className="product-title">{product.name}</h1>
+        <p className="product-price">€ {product.price}</p>
+        <p className="product-description">{product.description}</p>
+
+        <button className="product-btn">Aggiungi al carrello</button>
+
+        
+      </div>
+    </div>
   )
 }
