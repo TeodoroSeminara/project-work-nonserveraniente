0519d1a45b93712698cbe2e2957a93ca2f31a33b
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-11-18 19:46:31 +0100

    debug productcard

--- src/components/main/ProductCard.jsx
+++ src/components/main/ProductCard.jsx
@@ -1,43 +1,29 @@
-// import { Link } from "react-router-dom";
-// import "../../styles/ProductCard.css";
-
-// export function ProductCard({ product }) {
-//     return (
-//         <Link 
-//             to={`/product/${product.slug}`} 
-//             className="product-card-link"
-//             style={{ textDecoration: "none", color: "inherit" }}
-//         >
-//             <div className="product-card">
-//                 <div className="product-card-image-wrapper">
-//                     <img 
-//                         src={product.image_url} 
-//                         alt={product.name} 
-//                         className="product-card-image"
-//                     />
-//                 </div>
-
-//                 <h3 className="product-card-title">{product.name}</h3>
-//                 <p className="product-card-description">{product.description}</p>
-//                 <p className="product-card-price">{product.price}€</p>
-//             </div>
-//         </Link>
-//     );
-// }
-
-
 import { Link } from "react-router-dom";
 import "../../styles/ProductCard.css";
 
 export function ProductCard({ product }) {
     return (
-        <div className="product-card">
-            <div className="product-card-image-wrapper">
-                <img src={product.image_url} alt={product.name} className="product-card-image"/>
+        <Link 
+            to={`/product/${product.slug}`} 
+            className="product-card-link"
+            style={{ textDecoration: "none", color: "inherit" }}
+        >
+            <div className="product-card">
+                <div className="product-card-image-wrapper">
+                    <img 
+                        src={product.image_url} 
+                        alt={product.name} 
+                        className="product-card-image"
+                    />
+                </div>
+
+                <h3 className="product-card-title">{product.name}</h3>
+                <p className="product-card-description">{product.description}</p>
+                <p className="product-card-price">{product.price}€</p>
             </div>
-            <h3 className="product-card-title">{product.name}</h3>
-            <p className="product-card-description">{product.description}</p>
-            <p className="product-card-price">{product.price}€</p>
-        </div>
+        </Link>
     );
 }
+
+
+
