00d51922bcaaf53b6ee413bdf9f4363291c6454c
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-11-18 17:54:55 +0100

    Add notfound404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "../styles/NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <h2 className="notfound-subtitle">Pagina non trovata</h2>
+      <p className="notfound-text">
+        La pagina che stai cercando non esiste o è stata rimossa.
+      </p>
+
+      <Link to="/" className="notfound-btn">
+        Torna alla Home
+      </Link>
+    </div>
+  );
+}
